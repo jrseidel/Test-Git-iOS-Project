36d1fb16ed8d2a60c143166d6d236afbc8d6349a
Joe Seidel <joeseidel@Joe-Seidel-MacBook.local>
2014-04-29 13:21:11 -0400

    added a test class

---
+++ Git Test/DUQTest.h
@@ -0,0 +1,13 @@
+//
+//  DUQTest.h
+//  Git Test
+//
+//  Created by Joe Seidel on 4/29/14.
+//  Copyright (c) 2014 Joe Seidel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DUQTest : NSObject
+
+@end
---
+++ Git Test/DUQTest.m
@@ -0,0 +1,13 @@
+//
+//  DUQTest.m
+//  Git Test
+//
+//  Created by Joe Seidel on 4/29/14.
+//  Copyright (c) 2014 Joe Seidel. All rights reserved.
+//
+
+#import "DUQTest.h"
+
+@implementation DUQTest
+
+@end
